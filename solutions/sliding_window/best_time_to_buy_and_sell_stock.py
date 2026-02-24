ec62bbd328f7e6d06270cd8175a9bf15b24e163c
amistry08 <ayushmistry1998@gmail.com>
2026-02-24 10:20:57 -0500

    Day 6, Feb 24 26

---
+++ solutions/sliding_window/best_time_to_buy_and_sell_stock.py
@@ -0,0 +1,19 @@
+from typing import List
+
+class Solution:
+    def maxProfit(self, prices: List[int]) -> int:
+        profit = 0
+        
+        if len(prices) < 2:
+            return 0
+
+        i, j = 0, 1
+        while j < len(prices):
+            if prices[i] > prices[j]:
+                i += 1
+                j = i + 1
+            else:
+                profit = max(profit, prices[j]-prices[i])
+                j += 1
+
+        return profit
